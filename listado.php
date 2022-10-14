cbfa523344cfd7d1f75c8fd0f13a88aa4951b9b1
pcarcal254 <pcarcal254@g.educaand.es>
2022-10-14 08:29:14 +0200

    v2.0 Listado

--- listado.php
+++ listado.php
@@ -1,4 +1,7 @@
-<?php include('./bd/accesoADatos.php'); ?>
+<?php 
+include ("accesoABD.php");
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,9 +9,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LISTADO</title>
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Document</title>
 </head>
-<body> 
-    
+<body>
+    <div><h1>Listado</h1><a href="nuevo.php">Nuevo</a>
+</div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Foto</th>
+                    <th><img src="img/editar.png" href="Edita.php"></th>
+                    <th><img src="img/borrar.png" href="Nuevo.php"></th>
+                </tr>
+            </thead>
+            <tbody id="body">
+                <?php 
+                $registro=selectAll();
+                if($registro==0){
+                    echo "<tr><td colspan=5>No se ha encontrado ningún elemento</td></tr>";
+                }else{
+                    while($registro=$producto->fetch()){
+                        echo "<tr><td>".$registro['id']."</td><td>".$registro['nombre']."</td><td><img src='data:image/png;base64,".$registro['foto']."'></tr>";
+                    }
+                }
+                
+                ?>
+            </tbody>
+        </table>
 </body>
 </html>
